26f279bc9e0dc6e3c7e76be4b727ef241af09bd1
huaqiezi <1102603375@qq.com>
2025-01-07 11:56:12 +0800

    update

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,8 @@ import 'package:flutter_develop_template/main/application.dart';
 
 /// 生产环境 入口函数
 void main() => Application.runApplication(
-  envTag: EnvTag.prod, // 生产环境
-  platform: ApplicationPlatform.app, // 手机应用
-  baseUrl: 'https://www.wanandroid.com/', // 域名
-);
+      envTag: EnvTag.prod, // 生产环境
+      platform: ApplicationPlatform.app, // 手机应用
+      isGlobalNotification: true, // 是否有全局通知操作，比如切换用户
+      baseUrl: 'https://www.wanandroid.com/', // 域名
+    );
--- lib/main/main_pre.dart
+++ lib/main/main_pre.dart
@@ -4,5 +4,6 @@ import 'package:flutter_develop_template/main/application.dart';
 void main() => Application.runApplication(
       envTag: EnvTag.pre, // 预发布环境
       platform: ApplicationPlatform.app, // 手机应用
+      isGlobalNotification: true, // 是否有全局通知操作，比如切换用户
       baseUrl: 'https://www.wanandroid.com/', // 域名
     );
